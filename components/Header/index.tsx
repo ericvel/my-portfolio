4b7f751dd44dea6170af0e04d244928af4b34348
Eric Veliyulin <eric.vel@outlook.com>
2021-12-02 10:21:33 +0100

    Create global styles, create header and footer

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import EricVeliyulinLogo from "images/logo.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <p>This is the header</p>
+    </header>
+  );
+}
